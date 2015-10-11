bd6ab1060f9f83cf88871a9228c70639830f98cb
Smile0ff <a.parneta@gmail.com>
2015-10-11 23:23:52 +0300

    selectify fix

--- assets/js/lib/select.js
+++ assets/js/lib/select.js
@@ -1,55 +1,59 @@
-(($, root) => {
+(function($, root){
 
 	"use strict";
 
-	var _defaults = {};
+	var pluginName = "selectify",
+		_defaults = {};
 
-	class Plugin{
-		constructor(el, options){
-			this.el = $(el);
-			this._options = $.extend({}, _defaults, options);
-
-			this.initalize.apply(this, arguments);
-		}
+	function Plugin(el, options){
+		this.el = $(el);
+		this._options = $.extend({}, _defaults, options);
 		
-		initalize(){
-			this._events();
-		}
-		_events(){
-			this.el
-				.on("click", ".btn-select", $.proxy(this.openList, this))
-				.on("click", ".select-list", $.proxy(this.closeList, this))
-				.on("click", ".option-item", $.proxy(this.handleOption, this));
-		}
-		openList(e){
-			this.el.find(".select-list").addClass("active");
-			return false;
-		}
-		handleOption(e){
-			var	btnSelect = this.el.find(".btn-select"),
-				hdnInput = this.el.find("input[type=hidden]"),
-				target = $(e.target).closest(".option-item"),
-				optionID = parseInt(target.data("option")),
-				optionName = target.find(".option-name").text();
+		this.initialize.apply(this, arguments);
+	}
+	Plugin.prototype = {
+		initialize,
+		_events,
+		handleOption,
+		openList,
+		closeList
+	}
+	function initialize(){
+		this._events();
+	}
+	function _events(){
+		this.el
+			.on("click", ".btn-select", $.proxy(this.openList, this))
+			.on("click", ".select-list", $.proxy(this.closeList, this))
+			.on("click", ".option-item", $.proxy(this.handleOption, this));
+	}
+	function handleOption(e){
+		var	btnSelect = this.el.find(".btn-select"),
+			hdnInput = this.el.find("input[type=hidden]"),
+			target = $(e.target).closest(".option-item"),
+			optionID = parseInt(target.data("option")),
+			optionName = target.find(".option-name").text();
 
-			if(isNaN(optionID)) return;
-			hdnInput.val(optionID);
-			btnSelect.find(".text").text(optionName);
-			target.addClass("active").siblings(".option-item").removeClass("active");
-			this.closeList();
+		if(isNaN(optionID)) return;
+		hdnInput.val(optionID);
+		btnSelect.find(".text").text(optionName);
+		target.addClass("active").siblings(".option-item").removeClass("active");
+		this.closeList();
 
-			return false;
-		}
-		closeList(e){
-			this.el.find(".select-list").removeClass("active");
-		}
-		
+		return false;
+	}
+	function openList(e){
+		this.el.find(".select-list").addClass("active");
+		return false;
+	}
+	function closeList(e){
+		this.el.find(".select-list").removeClass("active");
 	}
 
-	$.fn.selectify = function(options){
+	$.fn[pluginName] = function(options){
 		return this.each(function(){
-			if(!$.data(this, "plugin-Selectify")){
-				$.data(this, "plugin-Selectify", new Plugin(this, options));
+			if(!$.data(this, "plugin-selectify")){
+				$.data(this, "plugin-selectify", new Plugin(this, options));
 			}
 		});
 	}
