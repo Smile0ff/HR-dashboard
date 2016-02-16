41acd5d26f45678600acac174d022af91e9a9d9c
Smile0ff <a.parneta@gmail.com>
2016-02-16 18:41:18 +0200

    registry

---
+++ assets/js/app/registry.js
@@ -0,0 +1,24 @@
+"use strict";
+
+import $ from "jquery";
+import validate from "jquery-validation";
+import mask from "jquery-mask-plugin";
+import selectify from "../lib/select";
+import Menu from "../controllers/menu";
+import Confirm from "../controllers/confirm";
+import Bind from "../controllers/bindController";
+import Status from "../controllers/statusPanel";
+import FilePreview from "../controllers/filePreview";
+
+
+$(function(){
+
+    new Menu();
+    new Bind();
+    new Status();
+    new Confirm();
+    new FilePreview();
+
+    $(".select-holder").selectify();
+    $("form").validate();
+});
